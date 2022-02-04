4da12e14bbfbf2568588ab0a1128f008aba4dc80
Himanshu Malik <jsm.28415@gmail.com>
2022-02-04 21:23:25 +0530

    importing all components files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+// import {Article, Brand, CTA, Feature, Navbar} from './components';
+
+// -> instead of writing this whole thing over here, we can just import it from src/index.js 
+// import Article from './components/Article/Article'
+// import Brand from './components/Brand/Brand'
+// import CTA from './components/CTA/CTA'
+// import Feature from './components/Feature/Feature'
+// import Navbar from './components/Navbar/Navbar'
+
 const App = () => {
   return( 
     <div>
